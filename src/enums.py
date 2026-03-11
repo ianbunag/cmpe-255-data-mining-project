2bf9e879624936e5d3f2f8d17173a658027f0f0c
Ian Buñag <jianbunag@gmail.com>
2026-03-11 00:00:36 -0700

    Pre narrow respiratory virus data features

--- src/enums.py
+++ src/enums.py
@@ -19,141 +19,140 @@ measurements: Measurements = {
 }
 
 
-class AirQualityKeys(TypedDict):
-    time: str
-    us_aqi: str
-    us_aqi_pm2_5: str
-    us_aqi_pm10: str
-    us_aqi_nitrogen_dioxide: str
-    us_aqi_carbon_monoxide: str
-    us_aqi_ozone: str
-    us_aqi_sulphur_dioxide: str
-    methane: str
-    uv_index_clear_sky: str
-    uv_index: str
-    dust: str
-    aerosol_optical_depth: str
-    ozone: str
-    sulphur_dioxide: str
-    nitrogen_dioxide: str
-    carbon_dioxide: str
-    pm2_5: str
-    carbon_monoxide: str
-    pm10: str
+AirQualityKeys = TypedDict('AirQualityKeys', {
+    'time': str,
+    'us_aqi (USAQI)': str,
+    'us_aqi_pm2_5 (USAQI)': str,
+    'us_aqi_pm10 (USAQI)': str,
+    'us_aqi_nitrogen_dioxide (USAQI)': str,
+    'us_aqi_carbon_monoxide (USAQI)': str,
+    'us_aqi_ozone (USAQI)': str,
+    'us_aqi_sulphur_dioxide (USAQI)': str,
+    'methane (μg/m³)': str,
+    'uv_index_clear_sky ()': str,
+    'uv_index ()': str,
+    'dust (μg/m³)': str,
+    'aerosol_optical_depth': str,
+    'ozone (μg/m³)': str,
+    'sulphur_dioxide (μg/m³)': str,
+    'nitrogen_dioxide (μg/m³)': str,
+    'carbon_dioxide (ppm)': str,
+    'pm2_5 (μg/m³)': str,
+    'carbon_monoxide (μg/m³)': str,
+    'pm10 (μg/m³)': str
+})
 
-
-air_quality_keys: AirQualityKeys = {
+aq_keys: AirQualityKeys = {
     'time': 'time',
-    'us_aqi': 'us_aqi (USAQI)',
-    'us_aqi_pm2_5': 'us_aqi_pm2_5 (USAQI)',
-    'us_aqi_pm10': 'us_aqi_pm10 (USAQI)',
-    'us_aqi_nitrogen_dioxide': 'us_aqi_nitrogen_dioxide (USAQI)',
-    'us_aqi_carbon_monoxide': 'us_aqi_carbon_monoxide (USAQI)',
-    'us_aqi_ozone': 'us_aqi_ozone (USAQI)',
-    'us_aqi_sulphur_dioxide': 'us_aqi_sulphur_dioxide (USAQI)',
-    'methane': 'methane (μg/m³)',
-    'uv_index_clear_sky': 'uv_index_clear_sky ()',
-    'uv_index': 'uv_index ()',
-    'dust': 'dust (μg/m³)',
+    'us_aqi (USAQI)': 'us_aqi (USAQI)',
+    'us_aqi_pm2_5 (USAQI)': 'us_aqi_pm2_5 (USAQI)',
+    'us_aqi_pm10 (USAQI)': 'us_aqi_pm10 (USAQI)',
+    'us_aqi_nitrogen_dioxide (USAQI)': 'us_aqi_nitrogen_dioxide (USAQI)',
+    'us_aqi_carbon_monoxide (USAQI)': 'us_aqi_carbon_monoxide (USAQI)',
+    'us_aqi_ozone (USAQI)': 'us_aqi_ozone (USAQI)',
+    'us_aqi_sulphur_dioxide (USAQI)': 'us_aqi_sulphur_dioxide (USAQI)',
+    'methane (μg/m³)': 'methane (μg/m³)',
+    'uv_index_clear_sky ()': 'uv_index_clear_sky ()',
+    'uv_index ()': 'uv_index ()',
+    'dust (μg/m³)': 'dust (μg/m³)',
     'aerosol_optical_depth': 'aerosol_optical_depth',
-    'ozone': 'ozone (μg/m³)',
-    'sulphur_dioxide': 'sulphur_dioxide (μg/m³)',
-    'nitrogen_dioxide': 'nitrogen_dioxide (μg/m³)',
-    'carbon_dioxide': 'carbon_dioxide (ppm)',
-    'pm2_5': 'pm2_5 (μg/m³)',
-    'carbon_monoxide': 'carbon_monoxide (μg/m³)',
-    'pm10': 'pm10 (μg/m³)',
+    'ozone (μg/m³)': 'ozone (μg/m³)',
+    'sulphur_dioxide (μg/m³)': 'sulphur_dioxide (μg/m³)',
+    'nitrogen_dioxide (μg/m³)': 'nitrogen_dioxide (μg/m³)',
+    'carbon_dioxide (ppm)': 'carbon_dioxide (ppm)',
+    'pm2_5 (μg/m³)': 'pm2_5 (μg/m³)',
+    'carbon_monoxide (μg/m³)': 'carbon_monoxide (μg/m³)',
+    'pm10 (μg/m³)': 'pm10 (μg/m³)',
 }
 
+RespiratoryVirusKeys = TypedDict('RespiratoryVirusKeys', {
+    'SEASON': str,
+    'AGE_GRP': str,
+    'RPHO_REGION': str,
+    'WEEKENDING': str,
+    'MMWR_WEEK': str,
+    'MMWR_YEAR': str,
+    'COV_POSITIVES': str,
+    'COV_TOTAL_TESTS': str,
+    'COV_TP': str,
+    'COV_TP_LEVEL': str,
+    'FLU_POSITIVES': str,
+    'FLU_TOTAL_TESTS': str,
+    'FLU_TP': str,
+    'FLU_TP_LEVEL': str,
+    'RSV_POSITIVES': str,
+    'RSV_TOTAL_TESTS': str,
+    'RSV_TP': str,
+    'RSV_TP_LEVEL': str,
+    'FLU_A_TESTS': str,
+    'FLU_B_TESTS': str,
+    'COV_ED_VISITS': str,
+    'FLU_ED_VISITS': str,
+    'RSV_ED_VISITS': str,
+    'POP': str,
+    'COV_ADM': str,
+    'FLU_ADM': str,
+    'RSV_ADM': str,
+    'COV_ADM_RATE': str,
+    'FLU_ADM_RATE': str,
+    'RSV_ADM_RATE': str,
+    'COV_ADM_LEVEL': str,
+    'FLU_ADM_LEVEL': str,
+    'RSV_ADM_LEVEL': str,
+    'TOTAL_DEATHS': str,
+    'COV_DEATHS': str,
+    'FLU_DEATHS': str,
+    'RSV_DEATHS': str,
+    'COV_DEATHS_PER': str,
+    'FLU_DEATHS_PER': str,
+    'RSV_DEATHS_PER': str,
+    'SEASON_COV_PED_DEATHS': str,
+    'SEASON_FLU_PED_DEATHS': str,
+    'SEASON_RSV_PED_DEATHS': str
+})
 
-class RespiratoryVirusKeys(TypedDict):
-    season: str
-    age_group: str
-    rpho_region: str
-    weekending: str
-    mmwr_week: str
-    mmwr_year: str
-    cov_positives: str
-    cov_total_tests: str
-    cov_tp: str
-    cov_tp_level: str
-    flu_positives: str
-    flu_total_tests: str
-    flu_tp: str
-    flu_tp_level: str
-    rsv_positives: str
-    rsv_total_tests: str
-    rsv_tp: str
-    rsv_tp_level: str
-    flu_a_tests: str
-    flu_b_tests: str
-    cov_ed_visits: str
-    flu_ed_visits: str
-    rsv_ed_visits: str
-    pop: str
-    cov_adm: str
-    flu_adm: str
-    rsv_adm: str
-    cov_adm_rate: str
-    flu_adm_rate: str
-    rsv_adm_rate: str
-    cov_adm_level: str
-    flu_adm_level: str
-    rsv_adm_level: str
-    total_deaths: str
-    cov_deaths: str
-    flu_deaths: str
-    rsv_deaths: str
-    cov_deaths_per: str
-    flu_deaths_per: str
-    rsv_deaths_per: str
-    season_cov_ped_deaths: str
-    season_flu_ped_deaths: str
-    season_rsv_ped_deaths: str
-
-
-respiratory_virus_keys: RespiratoryVirusKeys = {
-    'season': 'SEASON',
-    'age_group': 'AGE_GRP',
-    'rpho_region': 'RPHO_REGION',
-    'weekending': 'WEEKENDING',
-    'mmwr_week': 'MMWR_WEEK',
-    'mmwr_year': 'MMWR_YEAR',
-    'cov_positives': 'COV_POSITIVES',
-    'cov_total_tests': 'COV_TOTAL_TESTS',
-    'cov_tp': 'COV_TP',
-    'cov_tp_level': 'COV_TP_LEVEL',
-    'flu_positives': 'FLU_POSITIVES',
-    'flu_total_tests': 'FLU_TOTAL_TESTS',
-    'flu_tp': 'FLU_TP',
-    'flu_tp_level': 'FLU_TP_LEVEL',
-    'rsv_positives': 'RSV_POSITIVES',
-    'rsv_total_tests': 'RSV_TOTAL_TESTS',
-    'rsv_tp': 'RSV_TP',
-    'rsv_tp_level': 'RSV_TP_LEVEL',
-    'flu_a_tests': 'FLU_A_TESTS',
-    'flu_b_tests': 'FLU_B_TESTS',
-    'cov_ed_visits': 'COV_ED_VISITS',
-    'flu_ed_visits': 'FLU_ED_VISITS',
-    'rsv_ed_visits': 'RSV_ED_VISITS',
-    'pop': 'POP',
-    'cov_adm': 'COV_ADM',
-    'flu_adm': 'FLU_ADM',
-    'rsv_adm': 'RSV_ADM',
-    'cov_adm_rate': 'COV_ADM_RATE',
-    'flu_adm_rate': 'FLU_ADM_RATE',
-    'rsv_adm_rate': 'RSV_ADM_RATE',
-    'cov_adm_level': 'COV_ADM_LEVEL',
-    'flu_adm_level': 'FLU_ADM_LEVEL',
-    'rsv_adm_level': 'RSV_ADM_LEVEL',
-    'total_deaths': 'TOTAL_DEATHS',
-    'cov_deaths': 'COV_DEATHS',
-    'flu_deaths': 'FLU_DEATHS',
-    'rsv_deaths': 'RSV_DEATHS',
-    'cov_deaths_per': 'COV_DEATHS_PER',
-    'flu_deaths_per': 'FLU_DEATHS_PER',
-    'rsv_deaths_per': 'RSV_DEATHS_PER',
-    'season_cov_ped_deaths': 'SEASON_COV_PED_DEATHS',
-    'season_flu_ped_deaths': 'SEASON_FLU_PED_DEATHS',
-    'season_rsv_ped_deaths': 'SEASON_RSV_PED_DEATHS',
+rv_keys: RespiratoryVirusKeys = {
+    'SEASON': 'SEASON',
+    'AGE_GRP': 'AGE_GRP',
+    'RPHO_REGION': 'RPHO_REGION',
+    'WEEKENDING': 'WEEKENDING',
+    'MMWR_WEEK': 'MMWR_WEEK',
+    'MMWR_YEAR': 'MMWR_YEAR',
+    'COV_POSITIVES': 'COV_POSITIVES',
+    'COV_TOTAL_TESTS': 'COV_TOTAL_TESTS',
+    'COV_TP': 'COV_TP',
+    'COV_TP_LEVEL': 'COV_TP_LEVEL',
+    'FLU_POSITIVES': 'FLU_POSITIVES',
+    'FLU_TOTAL_TESTS': 'FLU_TOTAL_TESTS',
+    'FLU_TP': 'FLU_TP',
+    'FLU_TP_LEVEL': 'FLU_TP_LEVEL',
+    'RSV_POSITIVES': 'RSV_POSITIVES',
+    'RSV_TOTAL_TESTS': 'RSV_TOTAL_TESTS',
+    'RSV_TP': 'RSV_TP',
+    'RSV_TP_LEVEL': 'RSV_TP_LEVEL',
+    'FLU_A_TESTS': 'FLU_A_TESTS',
+    'FLU_B_TESTS': 'FLU_B_TESTS',
+    'COV_ED_VISITS': 'COV_ED_VISITS',
+    'FLU_ED_VISITS': 'FLU_ED_VISITS',
+    'RSV_ED_VISITS': 'RSV_ED_VISITS',
+    'POP': 'POP',
+    'COV_ADM': 'COV_ADM',
+    'FLU_ADM': 'FLU_ADM',
+    'RSV_ADM': 'RSV_ADM',
+    'COV_ADM_RATE': 'COV_ADM_RATE',
+    'FLU_ADM_RATE': 'FLU_ADM_RATE',
+    'RSV_ADM_RATE': 'RSV_ADM_RATE',
+    'COV_ADM_LEVEL': 'COV_ADM_LEVEL',
+    'FLU_ADM_LEVEL': 'FLU_ADM_LEVEL',
+    'RSV_ADM_LEVEL': 'RSV_ADM_LEVEL',
+    'TOTAL_DEATHS': 'TOTAL_DEATHS',
+    'COV_DEATHS': 'COV_DEATHS',
+    'FLU_DEATHS': 'FLU_DEATHS',
+    'RSV_DEATHS': 'RSV_DEATHS',
+    'COV_DEATHS_PER': 'COV_DEATHS_PER',
+    'FLU_DEATHS_PER': 'FLU_DEATHS_PER',
+    'RSV_DEATHS_PER': 'RSV_DEATHS_PER',
+    'SEASON_COV_PED_DEATHS': 'SEASON_COV_PED_DEATHS',
+    'SEASON_FLU_PED_DEATHS': 'SEASON_FLU_PED_DEATHS',
+    'SEASON_RSV_PED_DEATHS': 'SEASON_RSV_PED_DEATHS',
 }

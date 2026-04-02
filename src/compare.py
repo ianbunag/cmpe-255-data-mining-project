644975032e07de2bd2144d91334fceec29bd8775
Ian Buñag <jianbunag@gmail.com>
2026-04-01 20:36:16 -0700

    Apply review comments

--- src/compare.py
+++ src/compare.py
@@ -12,10 +12,10 @@ PREFIX_LOG1P = 'log1p_'
 PREFIX_CBRT = 'cbrt_'
 PREFIX_YEO_JOHNSON = 'yeo_johnson_'
 
-def compare_transformations(df: pd.DataFrame, columns: list[str] = []):
+def compare_transformations(df: pd.DataFrame, columns: list[str] | None = None):
     df_transforms = df.copy()
-    if len(columns) == 0:
-        columns = df_transforms.columns
+    if not columns:
+        columns = list(df_transforms.columns)
 
     skew = {}
     skew_sqrt = {}
@@ -52,34 +52,34 @@ def compare_transformations(df: pd.DataFrame, columns: list[str] = []):
         'Yeo-Johnson Transform': skew_yeo_johnson,
     })
 
-def compare_skewness(df_original: pd.DataFrame, df_transformed: pd.DataFrame, transformations: dict):
+def compare_skewness(df_original: pd.DataFrame, df_target: pd.DataFrame, changes: dict):
     skew_original = {}
-    skew_transformed = {}
-    transformations = transformations.copy()
+    skew_target = {}
+    changes = changes.copy()
 
-    for column in df_transformed.columns:
-        if column in transformations:
-            if isinstance(transformations[column], Enum):
-                transformations[column] = transformations[column].value
+    for column in df_target.columns:
+        if column in changes:
+            if isinstance(changes[column], Enum):
+                changes[column] = changes[column].value
         else:
-            transformations[column] = 'None'
+            changes[column] = 'None'
 
-    for column in df_transformed.columns:
+    for column in df_target.columns:
         if not is_numeric_dtype(df_original[column]):
             skew_original[column] = 'N/A'
-            skew_transformed[column] = 'N/A'
+            skew_target[column] = 'N/A'
             continue
 
         skew_original[column] = round(df_original[column].skew(), 2)
-        skew_transformed[column] = round(df_transformed[column].skew(), 2)
+        skew_target[column] = round(df_target[column].skew(), 2)
 
     return pd.DataFrame({
-        'Transformation': transformations,
+        'Changes': changes,
         'Skewness (Original)': skew_original,
-        'Skewness (Transformed)': skew_transformed
+        'Skewness (Target)': skew_target
     })
 
-def compare_distributions(df_original: pd.DataFrame, df_transformed: pd.DataFrame, columns: list[str]):
+def compare_distributions(df_original: pd.DataFrame, df_target: pd.DataFrame, columns: list[str]):
     for column in columns:
         fig, axes = plt.subplots(2, 2, figsize=(12, 8))
 
@@ -88,16 +88,16 @@ def compare_distributions(df_original: pd.DataFrame, df_transformed: pd.DataFram
         axes[0, 0].set_title('Histogram (Original)')
         axes[0, 0].set_xlabel(column)
 
-        axes[0, 1].hist(df_transformed[column], alpha=0.6, bins=20, color='lightsalmon', edgecolor='black')
-        axes[0, 1].set_title('Histogram (Transformed)')
+        axes[0, 1].hist(df_target[column], alpha=0.6, bins=20, color='lightsalmon', edgecolor='black')
+        axes[0, 1].set_title('Histogram (Target)')
         axes[0, 1].set_xlabel(column)
 
         df_original.plot(y=[column], ax=axes[1, 0], title='Line chart (Original)', kind='line')
         sns.regplot(data=df_original, x=df_original.index, y=column, ax=axes[1, 0], scatter=False,
                     color='lightblue')
 
-        df_transformed.plot(y=[column], ax=axes[1, 1], title='Line chart (Log transformed)', kind='line')
-        sns.regplot(data=df_transformed, x=df_transformed.index, y=column, ax=axes[1, 1], scatter=False,
+        df_target.plot(y=[column], ax=axes[1, 1], title='Line chart (Target)', kind='line')
+        sns.regplot(data=df_target, x=df_target.index, y=column, ax=axes[1, 1], scatter=False,
                     color='lightblue')
 
         plt.tight_layout()

9f71112fc07d7ecd80c6792eb26dfe5db48a0e7d
Ian Buñag <jianbunag@gmail.com>
2026-04-02 17:49:27 -0700

    34-6: Find central tendency

--- src/eda.py
+++ src/eda.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+from pandas.api.types import is_numeric_dtype
 
 def understand_features(df: pd.DataFrame, measurements: dict, descriptions: dict) -> pd.DataFrame:
     df.info()
@@ -37,4 +38,42 @@ def assess_features(df: pd.DataFrame, measurements: dict) -> pd.DataFrame:
         'Measurement Type': measurements,
         'Duplicate Count': df_duplicates,
         'Duplicate Percentage': df_duplicate_percentages
+    })
+
+def find_central_tendency(df: pd.DataFrame, measurements: dict) -> pd.DataFrame:
+    means = {}
+    medians = {}
+    skews = {}
+    skew_directions = {}
+
+    for key in df.columns:
+        if not is_numeric_dtype(df[key]):
+            medians[key] = 'N/A'
+            means[key] = 'N/A'
+            skews[key] = 'N/A'
+            skew_directions[key] = 'N/A'
+            continue
+
+        means[key] = round(df[key].mean(), 2)
+        medians[key] = round(df[key].median(), 2)
+        skew = round(df[key].skew(), 2)
+        skews[key] = skew
+
+        if -0.5 <= skew <= 0.5:
+            skew_directions[key] = 'Symmetrical'  # Mean = Median
+        elif -1 <= skew < -0.5:
+            skew_directions[key] = 'Moderately Left Skewed'  # Mean < Median
+        elif skew < -1:
+            skew_directions[key] = 'Highly Left Skewed'  # Mean < Median
+        elif 0.5 < skew <= 1:
+            skew_directions[key] = 'Moderately Right Skewed'  # Mean > Median
+        elif skew > 1:
+            skew_directions[key] = 'Highly Right Skewed'  # Mean > Median
+
+    return pd.DataFrame({
+        'Measurement Type': measurements,
+        'Mean': means,
+        'Median': medians,
+        'Skewness': skews,
+        'Skew Direction': skew_directions
     })
